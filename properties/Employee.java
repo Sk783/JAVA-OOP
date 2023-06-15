8669b45ded52d9abed2b142feea06233d1be969d
Sharad K <76866661+Sk783@users.noreply.github.com>
2023-06-16 00:00:12 +0530

    Update Employee.java

--- properties/Employee.java
+++ properties/Employee.java
@@ -2,13 +2,14 @@ package com.sharad.properties;
 
 /* Write a Java program to create a class called Employee with methods called work() and getSalary().
  Create a subclass called HRManager that overrides the work() method and adds a new method called addEmployee().*/
+
 public class Employee {
     final int salary;
     public Employee(int salary){
-        this.salary = salary;
+        this.salary=salary;
     }
     public void work(){
-        System.out.println("working as an employee!");
+        System.out.println("Working as an Employee");
     }
     public int getSalary(){
         return salary;
@@ -25,14 +26,13 @@ public class Employee {
             System.out.println("\nAdding new Employee!");
         }
     }
+    public static void main(String[] args){
+        Employee empl = new Employee(60000);
+        HRManager mngr = new HRManager(89000);
 
-    public static void main(String[] args) {
-        Employee employee = new Employee(56000);
-        HRManager hrmanager = new HRManager(78090);
-        employee.work();
-        System.out.println("Employee salary: "+employee.salary);
-
-        hrmanager.work();
-        System.out.println("Manager salary: "+hrmanager.salary);
+        empl.work();
+        System.out.println("Employee salary: "+emp.salary);
+        mngr.work();
+        System.out.println("Manager salary: "+mgr.salary);
     }
 }
